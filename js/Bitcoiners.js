4eccb1094871ada24797ff7cea1c98d050701c78
Jason <jasonbohio@gmail.com>
2024-08-30 10:48:16 -0400

    nym

--- js/Bitcoiners.js
+++ js/Bitcoiners.js
@@ -1,28 +1,28 @@
 BitcoinH.OCCUPATION_INITIALS = {
     pleb: {
         plebs: 40,
-        food: 100,
+        food: 200,
         ostriches: 4,
         sats: 100,
         zappower: 2,
     },
     miner: {
         plebs: 10,
-        food: 60,
+        food: 120,
         ostriches: 3,
         sats: 500,
         zappower: 5,
     },
     developer: {
         plebs: 35,
-        food: 40,
+        food: 80,
         ostriches: 2,
         sats: 10,
         zappower: 10,
     },
     educator: {
         plebs: 50,
-        food: 95,
+        food: 180,
         ostriches: 4,
         sats: 120,
         zappower: 3,
